b72d15c04fa37d0e133c743dd23952b455d0ce6c
marin <marin.bouthemy@gmail.com>
2022-09-24 15:38:31 +0800

    Adding some toaster

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,18 @@ import type { NextPage } from "next";
 import React, { useState } from "react";
 import { Button } from "../components/Button";
 import { Game } from "../components/Game";
-import Countdown, { zeroPad} from 'react-countdown';
+import Countdown, { zeroPad } from 'react-countdown';
 import { finishGameAndGetMoneyWebThree, Play2EarnModal } from "play2earn";
-
+import { Board } from "../components/Board";
+import { TileMeta } from "../components/Tile";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface IPropsRenderer {
-    hours: number;
-    minutes: number;
-    seconds: number;
-    completed: boolean;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  completed: boolean;
 }
 
 /**
@@ -19,45 +22,70 @@ interface IPropsRenderer {
  */
 const Home: NextPage = () => {
   const [date, setDate] = useState<Date>(new Date());
+  const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
   const [isCountdownStarted, setIsCountdownStarted] = useState<boolean>(false);
+
+  const defaultTilesBegin: TileMeta[] = [
+    { id: 0, position: [0, 1], value: 2 },
+    { id: 1, position: [0, 2], value: 2 }
+  ]
 
   const handleRestart = () => {
     setDate(new Date());
-    console.log('Game ID', )
+    console.log('Game ID',)
   };
 
-
-  const testGameIDCreation = () => {
-    console.log('Game ID', new Date().toISOString());
-
+  const testToast = () => {
+    toast.success('hello');
   }
 
-  const handleOnComplete = () => {
-      // GameOver; the game is finished
-      console.log('You have lost!');
-  }
-
-  const handleSimulateLosing = () => {
+  /**
+   * The user has won the game, the money is transferred on the backend.
+   * The signature is also displayed to the frontend.
+   */
+  const handleWinGame = () => {
+    toast.success("Congratulation for winning, wait a bit until the money is transferred to your account", {
+      autoClose: 10000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+    })
+    // TODO: Change the backend to return the signature of the game
     finishGameAndGetMoneyWebThree(process.env.NEXT_PUBLIC_WEBSITE_HOST || '2048-to-earn.web-2-to-3.com', date.toISOString(), 'game_master', 'game_master', false)
-    .then(resultSignature => {
-        console.log('The money has been transferred to your account, the signature is: ', resultSignature);
-    });
-
+      .then(resultSignature => {
+        toast.success(`The money has been transferred to your account, the signature is ${resultSignature}`, {
+          autoClose: 10000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+        })
+      });
   }
+
+  /**
+   * The user has lost, the money is transferred to the game master.
+   */
+  const handleOnComplete = () => {
+    console.log('You have lost!');
+    toast.warning("Unfortunately you didn't win this type, reload the page to try again!")
+    finishGameAndGetMoneyWebThree(process.env.NEXT_PUBLIC_WEBSITE_HOST || '2048-to-earn.web-2-to-3.com', date.toISOString(), 'game_master', 'game_master', false)
+      .then(resultSignature => {
+        console.log('The money has been transferred to the game master account, the signature is: ', resultSignature);
+      });
+  }
+
 
   // Renderer callback with condition
   const renderer = ({ hours, minutes, seconds, completed }: IPropsRenderer) => {
-      if (completed) {
+    if (completed) {
       // Render a completed state
-      return  <span>Game Over!</span>;;
-      } else {
+      return <span>Game Over!</span>;;
+    } else {
       return <span>{zeroPad(minutes)}:{zeroPad(seconds)}</span>;
-      }
+    }
   }
-  
-  const handleWinGame = () => {
-      console.log('Game ID is finished');
-  }
+
+
 
   /**
    * Handle the start of the game.
@@ -65,6 +93,8 @@ const Home: NextPage = () => {
    */
   const handleGameStarting = () => {
     console.log('Start the game');
+    toast("Betting worked! Game is starting, good luck!")
+    setIsGameStarted(true);  // Switch to dynamic board
     setIsCountdownStarted(true);
   }
 
@@ -76,37 +106,35 @@ const Home: NextPage = () => {
         </div>
       </div>
       <p>
-          <b>2048 2 Earn</b> is a play2earn where you can win Solana if you manage to beat the game within 20seconds.
+        <b>2048 To Earn</b> is a play2earn where you can win Solana if you manage to beat the game within 20 seconds.
           You first need to connect a Solana wallet and wage the amount of SOL. The game is currently only available on Devnet.
       </p>
-      {isCountdownStarted && 
-        <Countdown date={Date.now() + 5000}
-                   renderer={renderer} 
-                   onComplete={() => handleOnComplete()}
-                   />
+      {!isGameStarted &&
+        <Play2EarnModal gameWebsiteHost={process.env.NEXT_PUBLIC_WEBSITE_HOST || '2048-to-earn.com'}
+          gameID={date.toISOString()}
+          playerUID={'player1'}
+          handleGameStarting={() => handleGameStarting()}
+          gameType="solo"
+          blockchainType="solana"
+          amountBet={0.1} />
       }
-      <Game key={date.toISOString()} handleWinGame={() => handleWinGame()}/>
-      <div>
-        <p>
-          This game (2048) was built using <b>React</b> and <b>TypeScript</b>.
-          The unique part of this example is animations. The animations in React
-          aren't that straightforward, so I hope you can learn something new
-          from it.
-        </p>
-      </div>
-      <Play2EarnModal gameWebsiteHost={process.env.NEXT_PUBLIC_WEBSITE_HOST || '2048-to-earn.com'} 
-                                        gameID={date.toISOString()}
-                                        playerUID={'player1'} 
-                                        handleGameStarting={() => handleGameStarting()}
-                                        gameType="solo"
-                                        blockchainType="solana"
-                                        amountBet={0.1}/>
+
+      {isCountdownStarted &&
+        <Countdown date={Date.now() + 5000}
+          renderer={renderer}
+          onComplete={() => handleOnComplete()}
+        />
+      }
+      {isGameStarted ?
+        <Game key={date.toISOString()} handleWinGame={() => handleWinGame()} /> :
+        <Board tiles={defaultTilesBegin} tileCountPerRow={4} />
+      }
       <Button onClick={handleGameStarting}>Start Game</Button>
 
       <Button onClick={handleRestart}>Restart</Button>
-      <Button onClick={handleSimulateLosing}>Simulate Losing</Button>
-      <Button onClick={testGameIDCreation}>Test Game Creation</Button>
+      <Button onClick={handleOnComplete}>Simulate Losing</Button>
 
+      <ToastContainer />
       <div className="footer">
         Copyrights 2022 - 2048 To Earn
       </div>
